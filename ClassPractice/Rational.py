4ab3911250b00799e6015d6198abfb8dcf00d225
ThePhantomAlpha <will.h2o2@gmail.com>
2016-03-10 10:16:35 -0800

    kxzjsglkj
    zdjtslkvuds;iocyx3dlm

--- ClassPractice/Rational.py
+++ ClassPractice/Rational.py
@@ -1,3 +1,5 @@
+import math
+
 class Rational:
     def __init__(self, num, denom):
         self.numerator = num
@@ -28,12 +30,26 @@ class Rational:
         val = str(self.numerator) + "/" + str(self.denominator)
         return val
 
-
+    def simplify(self):
+        done = True
+        output = 1
+        minimum = min(self.numerator, self.denominator)
+        for num in range(minimum,1,-1):
+            output = num
+            if (self.numerator%num == 0) and (self.denominator%num==0):
+                break
+        return Rational(self.numerator/output,self.denominator/output)
 
 r1 = Rational(1,2)
 r2 = Rational(1,3)
 r3 = r1 + r2
 r4 = Rational(5,6)
+r5 = Rational(10,20)
+r6 = Rational(100,50)
+r7 = Rational(1,6)
 print r1 == r2
 print r3 == r4
 print r3
+print r5.simplify()
+print r6.simplify()
+print r7.simplify()
