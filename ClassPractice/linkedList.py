894a7f9afe8e3ea13c7d3148b761cacda992edf2
ThePhantomAlpha <will.h2o2@gmail.com>
2016-05-16 10:54:36 -0700

    LinkedList
    Did the linked list stuff

---
+++ ClassPractice/linkedList.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        count = 0
+        current = self.head
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        found = False
+        current = self.head
+        while not found and current is not None:
+            if current.getData() == item:
+                found = True
+            current = current.getNext()
+        return found
+
+    def remove(self, item):
+        found = False
+        current = self.head
+        prev = None
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                prev = current
+                current = current.getNext()
+        if prev is None:
+            self.head = current.getNext()
+        else:
+            prev.setNext(current.getNext())
+
+link = LinkedList()
+link.add(10)
+link.add(3)
+link.add(1345)
+link.add(46)
+print link.size()
+link.remove(3)
+print link.size()
